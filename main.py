a97bf6844431ed906aeda55f00ce7a40962fa88f
Pennell <21308@fraser.school.nz>
2024-05-01 10:05:56 +1200

    Commit 0105

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-import time
-
 # Ask for users budget.
 budget = input("What is your budget?")
 while not budget.isnumeric():
     print("Sorry we really need to know your budget to help you.")
     budget = input("What is your budget?")
 budget = int(budget)
-print("Budget: $")
+
+print("Your total budget is: $", budget)
+
+# Ask user for the product name, weight in kg and the cost.
+print("Can you please list the product name, weight in kg and the cost you wish to get price compared?\n")
+reply = input ("List them here:")
+
+
+
    
