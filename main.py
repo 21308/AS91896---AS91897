2cb7e8ab15e458cdf40118df14ce26cc62bbce9c
Pennell <21308@fraser.school.nz>
2024-05-01 10:32:00 +1200

    Wednesday 1 May 22024

--- main.py
+++ main.py
@@ -7,9 +7,18 @@ budget = int(budget)
 
 print("Your total budget is: $", budget)
 
-# Ask user for the product name, weight in kg and the cost.
+# Ask user for their inputs
 print("Can you please list the product name, weight in kg and the cost you wish to get price compared?\n")
-reply = input ("List them here:")
+# Ask for the product
+product_name = input ("Product Name:\n")
+# Ask for the weight of the product.
+weight = input ("Weight(kg)\n")
+# Ask for the cost of the product.
+cost = input ("Cost:\n")
+
+
+
+
 
 
 
