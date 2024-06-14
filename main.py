21acade5aa7b68a9822502466378a6bd0f0e913b
Pennell <21308@fraser.school.nz>
2024-06-14 13:59:31 +1200

    Friday 14 June 2024

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+# Introduce what this code is..
 print("This is a price comparison tool that will convert the price and weight to calculate the unit price. We will first ask for your budget range,\
  then we will ask you for your desired items and then do the calculations to find the unit price.\n")
+thisdict={}
+product_dict={}
+
 
 # Ask for users budget.
 budget = input("What is your budget?")
@@ -8,7 +12,6 @@ while not budget.isnumeric():
     budget = input("What is your budget?")
 budget = int(budget)
 
-thisdict={}
 
 
 print("Your total budget is: $", budget)
@@ -27,21 +30,25 @@ while True:
     # Ask for the cost of the product.
     print("Could you please list the cost of the item so that we can help you?\n")
     cost = int(input ("Cost($):\n"))
+    new_product = {product_name:cost}
+    product_dict.update(new_product)
     cost_per_kg=cost/weight
 
 
 # Storing users input in a dictionary
     new_product = {product_name:cost_per_kg}
     thisdict.update(new_product)
-
-
-print(thisdict)
-
 # Sorted Dictionary
 sorted_dict=dict(sorted(thisdict.items(), key=lambda item: item[1]))
-print(sorted_dict)
 
 
 # Recommending user the most suitable item from their inputs
-
-cheapest_price = sorted_dict.items()
+i=0
+while True:
+    cheapest_product = list(sorted_dict.keys())[i]
+    cheapest_cost = product_dict[cheapest_product]
+    
+    if cheapest_cost <= budget:
+        print(f"The most cost efficient product within your budget is {cheapest_product} for ${cheapest_cost}.")
+        break
+    i+=1
